b5e6e66dcbef438d185ffecac60c60a67cfd3110
unknown <pasanmahee.roo@gmail.com>
2020-03-13 10:17:18 +0530

    state change according to select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,15 +11,33 @@ export default App;*/
 import React, { Component } from "react";
 
 class App extends Component {
+    constructor(){
+        super();
+
+        this.state={
+            type: 'initial state'
+        }
+    }
+
+    onSelectTypeChange(event){
+        this.setState({
+            type: event.target.value
+        })
+    }
+
+
 
     render(){
+        console.log('this is the state', this.state)
+
+
         return(
             <div className="container">
                 <div>
                 <h1>Pokemon gotta catch em all</h1>
                 <form action="">
                     <label htmlFor="">Select Your Pokemon</label>
-                    <select name="" id="">
+                    <select name="" id="" onChange={this.onSelectTypeChange.bind(this)}>
                         <option value="1">Water</option>
                         <option value="2">Fighting</option>
                         <option value="3">Flying</option>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import test from './test.js';
 import App from './components/App';
 
 ReactDOM.render(
